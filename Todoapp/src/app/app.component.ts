a13bf7a997f263572a0ba6777e089d13776f6830
Nihashini Venugopal <nihashini@agiratech.com>
2019-08-05 17:45:13 +0530

    Completed todo app on angular

--- Todoapp/src/app/app.component.ts
+++ Todoapp/src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ChangeDetectorRef} from '@angular/core';
+import { NgForm,  } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent { 
   todoArray=[];
-  addTodo(value){    
-    this.todoArray.push(value) ;   
-    console.log(this.todoArray)  ;
+  title:any;
+ 
+  constructor(){}
+  addTodo(forms:NgForm){   
+   this.todoArray.push(forms) ; 
+     }
+   
+  deleteItem(todo){   
+    const index: number = this.todoArray.indexOf(todo);
+    if (index !== -1) {
+      this.todoArray.splice(this.todoArray.indexOf(todo),1);
+    } 
   } 
 }
