a7aa9454fe2aee513320213ae23e235160a6b251
Nihashini Venugopal <nihashini@agiratech.com>
2019-11-08 17:44:36 +0530

    did the totododone application without the inbuilt function

--- tododone/src/app/app.component.ts
+++ tododone/src/app/app.component.ts
@@ -7,24 +7,60 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/dra
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
   todo = [];
   done = [];
+  index: number;
   
   addTodo(forms:NgForm){ 
     this.todo.push(forms) ; 
-    console.log(this.todo);
+    console.log(this.todo)
+    }
+      todoDrop(event:CdkDragDrop<string[]>){
+        
+//        console.log('coming toDo event', event)
+
+        this.todo.splice(event.currentIndex, 0 ,event.container.data);
+        
+        this.done.splice(event.currentIndex, 1);
+
+        // console.log('event.container.datatodo',event.container.data)
+
       }
-      drop(event: CdkDragDrop<string[]>) {
-        if (event.previousContainer === event.container) {
-          moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-        } 
-        else {
-          transferArrayItem(event.previousContainer.data,
-                            event.container.data,
-                            event.previousIndex,
-                            event.currentIndex);
-        }
+
+      doneDrop(event:CdkDragDrop<string[]>){
+        console.log('coming done event', event) 
+        
+        this.done.splice(event.currentIndex, 0, event.container.data);
+        
+        this.todo.splice(event.currentIndex, 1);
+        
+        // console.log('event.container.datadone',event.container.data)
+
       }
-  }
+    }
+    
+    
+
+
+
+
+
+
+
+
+
+ // drop(event: CdkDragDrop<string[]>) {
+
+      //   if (event.previousContainer === event.container) {
+      //     // moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+          
+      //   } 
+      //   else {
+      //   //   transferArrayItem(event.previousContainer.data,
+      //   //                     event.container.data,
+      //   //                     event.previousIndex,
+      //   //                     event.currentIndex);
+      //   // }
+      // }}
+      
 
